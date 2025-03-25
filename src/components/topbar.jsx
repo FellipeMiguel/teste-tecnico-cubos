1bade552b6ceef409fd62a6be1afa91651869b5b
FellipeMiguel <fellioemiguel@gmail.com>
2025-03-25 07:56:56 -0300

    feat: alterando o Link para página home

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -6,14 +6,14 @@ const Topbar = () => {
   return (
     <header className=" bg-[#121113] backdrop-blur-[2px] py-4 border-b-1 border-[#49474E]">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-white">
+        <Link to="/" className="text-white cursor-pointer">
           <img src={logoImg} alt="" className="" />
-        </div>
-        <Link to="/" className="bg-[#B744F714] hover:bg-[#8E4EC6] flex">
+        </Link>
+        <div className="bg-[#B744F714] hover:bg-[#8E4EC6] flex">
           <button className="cursor-pointer rounded-sm px-5 py-3">
             <img src={sunImg} alt="" className="" />
           </button>
-        </Link>
+        </div>
       </div>
     </header>
   );
